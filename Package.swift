afd469a8d10bb049fb59384b28dad700d7d6fec8
Oscar Bouwmans <o.bouwmans@bexter.nl>
2024-10-25 14:41:00 +0200

    Set lower platform versions

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MIDIStreamParser",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
+        .macOS(.v10_13),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
